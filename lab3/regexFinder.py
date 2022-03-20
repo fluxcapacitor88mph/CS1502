aa3d83c40b39bf9af164b71e3e865d8e6ff5c4ff
jrl142 <jrl142@pitt.edu>
2022-03-20 01:27:29 -0400

    Initial Skeleton File

---
+++ lab3/regexFinder.py
@@ -0,0 +1,120 @@
+# John Lovre   - JRL142@pitt.edu
+# Suchi Attota - SUA27@pitt.edu
+# CS1502
+# PA3: Regex Pattern Recognition
+
+# import necessary libraries
+import sys, fileinput, os.path, bisect
+
+# define data structures
+class NFA: 
+	def _init_(self, numStates, alphabet, tranFunctions, startState, acceptStates):
+		self.numStates = numStates
+		self.alphabet = alphabet
+		self.tranFunctions = tranFunctions
+		self.startState = startState
+		self.acceptStates = acceptStates
+
+class DFA:
+	def _init_(self, numStates, alphabet, tranFunctions, startState, acceptStates):
+		self.numStates = numStates
+		self.alphabet = alphabet
+		self.tranFunctions = tranFunctions
+		self.startState = startState
+		self.acceptStates = acceptStates
+
+
+# 0) For this program you will read an alphabet and a regular expression from a file, and then
+# 	determine if a series of strings belong to the language of the regular expression. You will do
+# 	this by implementing the following algorithm:
+
+# 1) Convert the regular expression to an equivalent NFA, using the algorithm described in
+# 	class and in the textbook.
+
+# 2) Convert the NFA into an equivalent DFA, using the algorithm described in class and
+# 	in the textbook. Here, you can use the code you wrote for PA2, but you will not write
+# 	the DFA to a file - keep it in memory.
+
+# 3) For each of the strings, determine if it is in the language of the DFA by simulating the
+# 	DFA on the string. Here, you can use the code you wrote of PA1, but you will get the
+# 	DFA from memory, and not from a file. You will write the results to a file, which will
+# 	have one line per string, indicating if the string is ("true") or is not ("false") in the
+#	language of the regular expression.
+
+.
+.
+.
+
+# The program should read input from a fie that is specifed as the first com-
+# mand line argument to the program, and write output to a file whose name is
+# specified as the second command line argument to the program. The program
+# should not prompt the user for interactive input.
+
+##########################
+# Check for input errors #
+##########################
+# Command line should have 3 arguments (regexFinder.py, test_case, and output_file)
+#  Execute format: python3 regexFinder.py <testcase>.txt <output>.txt
+numArgs = len(sys.argv)
+if (numArgs != 3):
+	sys.exit("INPUT ERROR:\n Format should be \"python3 regexFinder.py <testcase>.txt <output>.txt\"")
+
+# Check that test file exists
+inputFilename = sys.argv[1]
+fileExists = os.path.exists(inputFilename)
+if (fileExists == False):
+	sys.exit("INPUT ERROR:\n Cannot find file. Check spelling of input.")
+
+# Create output file
+outputFilename = sys.argv[2]
+
+
+########################
+# Read input from file #
+########################
+# Note: Program reads in newline (\n) character from input files
+#	parsing steps check to remove '\n' from strings and arrays
+
+#  0) The input file name is the first command line argument.
+inputFile = open(inputFilename, "r")
+
+
+# 1)The alphabet of the language appears by itself on the fist line of the input file. Every
+#	character in the line (not including the terminating newline character, or any space
+#	characters) is a symbol of the alphabet. The alphabet cannot include the letter e,
+#	the letter N, the symbol *, the symbol j, or the left or right parenthesis, as these
+#	have specific meanings in the regular expressions. See below for more on the format
+#	of regular expressions. (Note: in PA1 and PA2, the space character could be in the
+#	alphabet of the language, but here for this assignment we disallow it. This is so
+#	that spaces can be put into regular expressions, to improve readability, without being
+#	treated as symbols.)
+
+
+# 2) A regular expression appears by itself on the second line of the input file.
+
+
+#  3) Following the regular expressions are a sequence of strings, one per line for the remain-
+#	der of the input file.
+
+
+inputFile.close()
+
+
+########################
+# Write to Output File #
+########################
+
+# 0) The output file name is the second command line argument.
+with open(outputFilename, 'w') as outFile:
+
+# 1) Your program should write true or false for each string in the input file,
+# 	true if the string is in the language described by the regular expression,
+# 	and false if not. The output file should contain one true or false value per
+# 	line.
+
+# 2) If an invalid regular expression is encountered, your program should print
+# 	"Invalid expression" to the output file on a single line. Nothing else should
+# 	be printed to the file.
+	
+
+outFile.close()
