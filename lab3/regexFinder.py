677ba784f5407cea04ff261ba7e5e974605d5908
jrl142 <jrl142@pitt.edu>
2022-03-27 01:14:57 -0400

    regex --> nfa conversion code started

--- lab3/regexFinder.py
+++ lab3/regexFinder.py
@@ -123,6 +123,64 @@ myNFA = {}
 
 #<conversion code goes here>#
 
+#Here is how you will convert the regular expression into an equivalent NFA:
+
+# 1. Parse the regular expression into an abstract syntax tree. In an abstract syntax tree,
+# the interior nodes represent the operators, and the leaf nodes represent symbols in the
+# alphabet. The children of the interior nodes are the operand(s) of the operator. Here
+# is how you will construct the syntax tree:
+
+#	(a) Create two initially empty stacks: a operand stack that will contain references to
+#		nodes in the syntax tree; and an operator stack that will contain operators (plus
+#		the left parenthesis).
+operands = []
+operators = []
+
+#	(b) Scan the regular expression character by character, ignoring space characters.
+#		i. If a symbol from the alphabet is encountered, then create a syntax tree node
+#			containing that symbol, and push it onto the operand stack.
+for ch in regex:
+	if (ch in alphabet):
+		operands.append(ch)
+
+#		ii. If a left paren is encountered, then push it onto the operator stack.
+
+#		iii. If an operator (star, union, or implied concatenation) is encountered, then,
+#			as long as the stack is not empty, and the top of the stack is an operator
+#			is precedence is greater than or equal to the precedence of the operator just
+#			scanned, pop the operator offthe stack and create a syntax tree node from it
+#			(popping its operand(s) off the operand stack), and push the new syntax tree
+#			node back onto the operand stack. When either the stack is empty, or the
+#			top of the stack is not an operator with precedence greater than or equal to
+#			the precedence of the operator just scanned, push the operator just scanned
+#			onto the operator stack.
+
+#		iv. If a right parenthesis is encountered, then pop operators off the operator stack
+#			until the left parenthesis is popped off the operator stack. For each operator
+#			popped off the stack, create a new syntax tree node from it (popping its
+#			operand(s) off the operand stack), and push it onto the operand stack.
+
+#	(c) Empty the operator stack. For each operator popped off the stack, create a new
+#		syntax tree node from it (popping its operand(s) off the operand stack), and push
+#		it onto the operand stack.
+
+#	(d) Pop the root of the syntax tree off of the operand stack.
+
+#	(e) If any problems are encountered that indicate an invalid expression, then termi-
+#		nate parsing an print the error message to the output file as described above.
+
+# 2. Create an NFA from the abstract syntax tree by doing a depth-first traversal of the
+#	syntax tree. (Remember here that each node of the syntax tree is the root of a sub-
+#	tree that represents a regular expression.) For each node, an NFA is created that is
+#	equivalent to the regular expression represented by the subtree rooted at the node. If
+#	the node is a leaf node, then we have a base case, and the NFA is straightforward to
+#	create. If the node is an interior node (representing an operator), then the NFA is
+#	created from the NFA's of the child nodes using the constructions described in section
+#	1.2 of the text (under \closure under the regular operations").
+
+# 3. And now you have an NFA equivalent to the regular expression.
+
+
 #<...in the meantime...dummy variables below>###################################
 numStates = 7        # size of Q (number of states in set)
 alphabet =  ['d', 'g']        # Sigma
@@ -278,6 +336,9 @@ with open(outputFilename, 'w') as outFile:
 # 2) If an invalid regular expression is encountered, your program should print
 # 	"Invalid expression" to the output file on a single line. Nothing else should
 # 	be printed to the file.
+
+
+# <code goes here>
 	
 
 outFile.close()
@@ -304,7 +365,8 @@ print("Test line\n NFA numState: "+str(myNFA.numStates))
 #print()
 print("Test line\n NFA alphabet: "+str(myNFA.alphabet))
 #print()
-print("Test line\n NFA tranFunctions: "+str(myNFA.tranFunctions))
+print("Test line\n NFA tranFunctions: ")
+print(str(myNFA.tranFunctions))
 #print()
 print("Test line\n NFA startState: "+str(myNFA.startState))
 #print()
